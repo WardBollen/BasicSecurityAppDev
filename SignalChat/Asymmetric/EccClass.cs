f6bc09a83f894a1b95c4cf281807a7065e566faa
Ward Bollen <ward.voorschool@outlook.com>
2019-03-19 13:35:58 +0100

    started working on Asymmetric project

---
+++ SignalChat/Asymmetric/EccClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using Chilkat;
+
+namespace Asymmetric
+{
+    class EccClass
+    {
+        public static string NewKeypair()
+        {
+            Ecc ecc=new Ecc();
+        }
+    }
+}
