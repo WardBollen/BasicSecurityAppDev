f241d4f380e880ada06065089392f47bf584aba6
Michaël Meeus <Michael.meeus@gmail.com>
2019-04-14 10:07:54 +0200

    Added hasher to ChatClient

---
+++ SignalChat/ChatClientCS/Encryption/Hasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Hashing
+{
+    class Hasher
+    {
+        private static SHA256CryptoServiceProvider csp = new SHA256CryptoServiceProvider();
+
+        public static byte[] hashText(string plainText)
+        {
+            var data = Encoding.Unicode.GetBytes(plainText);
+            return csp.ComputeHash(data);
+        }
+
+        public static byte[] hashBytes(byte[] bytes)
+        {
+            return csp.ComputeHash(bytes);
+        }
+    }
+}
